c47433dea157a16e83fa728098aed588833212a8
BORGES <jessborgesb@gmail.com>
2026-04-08 18:36:50 +0200

    feat: add User model - stores user data only (SRP)

---
+++ Tasca-S3-0/S/src/Model/User.java
@@ -0,0 +1,44 @@
+package Model;
+
+/**
+ * Represents a user in the system.
+ * SRP: responsible only for storing user data.
+ * Validation, email sending and registration logic
+ * are handled by dedicated classes.
+ */
+public class User {
+    private String name;
+    private String email;
+    private String password;
+
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
