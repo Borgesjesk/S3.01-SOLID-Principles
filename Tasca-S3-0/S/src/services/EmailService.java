d67fbd452bf98a73edbb886ccc1a7ce8889d622c
BORGES <jessborgesb@gmail.com>
2026-04-08 19:21:52 +0200

    feat: add EmailService (SRP)

---
+++ Tasca-S3-0/S/src/services/EmailService.java
@@ -0,0 +1,17 @@
+package services;
+
+/**
+ * Handles all email communication with users.
+ * SRP: responsible only for sending emails.
+ */
+public class EmailService {
+
+    public void sendEmail(String email, String subject, String body) {
+        System.out.println("📧 Sending email to " + email);
+        System.out.println("Subject: " + subject);
+        System.out.println("Body: " + body);
+    }
+    public void sendConfirmationEmail(String email) {
+        System.out.println("📧 Sending confirmation email to: " + email);
+    }
+}
