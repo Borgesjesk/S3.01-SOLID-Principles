0d338e99b5e6eafa0bee30245ac306eb0533ab67
BORGES <jessborgesb@gmail.com>
2026-04-08 21:15:49 +0200

    feat: add MainD entry point (DIP)

---
+++ Tasca-S3-0/D/src/MainD.java
@@ -0,0 +1,25 @@
+import interfaces.PersonRepository;
+import model.Person;
+import repository.MySqlPersonRepository;
+import services.ServicePerson;
+
+/**
+ * Entry point for the Dependency Inversion Principle exercise.
+ * Wires dependencies and demonstrates saving a person via abstraction.
+ */
+public class MainD {
+
+    public static void main(String[] args) {
+
+        // Abstraction
+        PersonRepository repository = new MySqlPersonRepository();
+
+        // Injection
+        ServicePerson servicePerson = new ServicePerson(repository);
+
+        Person person = new Person();
+        person.setName("Jonas");
+
+        servicePerson.savePerson(person);
+    }
+}
