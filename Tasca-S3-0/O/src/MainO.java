f830acd6251011c6231c0385eb4c746a5f3f4e09
BORGES <jessborgesb@gmail.com>
2026-04-09 11:20:15 +0200

    feat: add MainO entry point (OCP)

---
+++ Tasca-S3-0/O/src/MainO.java
@@ -0,0 +1,28 @@
+import interfaces.Instrument;
+import player.InstrumentPlayer;
+import services.Drums;
+import services.Guitar;
+import services.Piano;
+
+/**
+ * Entry point for the Open/Closed Principle exercise.
+ * Demonstrates adding new instruments without modifying InstrumentPlayer.
+ */
+public class MainO {
+
+    public static void main(String[] args) {
+
+        Instrument guitarService = new Guitar();
+        Instrument pianoService = new Piano();
+        Instrument drumsService = new Drums();
+
+        InstrumentPlayer playGuitarService = new InstrumentPlayer(guitarService);
+        InstrumentPlayer playPianoService = new InstrumentPlayer(pianoService);
+        InstrumentPlayer playDrumsService = new InstrumentPlayer(drumsService);
+
+        playGuitarService.play();
+        playPianoService.play();
+        playDrumsService.play();
+    }
+}
+
